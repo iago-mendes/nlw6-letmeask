03959b9bb535cbff5b2c74b656ebef04184f8813
Iago <iagobrazmendes@gmail.com>
2021-06-25 15:31:27 -0300

    chore: move route to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
-import {Home} from './pages/Home'
-import {NewRoom} from './pages/NewRoom'
 import {AuthContextProvider} from './contexts/AuthContext'
-import {Room} from './pages/Room'
-import {AdminRoom} from './pages/AdminRoom'
 import {ThemeProvider} from './contexts/Theme'
+import {Routes} from './Routes'
 
 function App() {
 	return (
 		<BrowserRouter>
 			<ThemeProvider>
 				<AuthContextProvider>
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route path="/rooms/new" component={NewRoom} />
-						<Route path="/rooms/:id" component={Room} />
-						<Route path="/admin/rooms/:id" component={AdminRoom} />
-					</Switch>
+					<Routes />
 				</AuthContextProvider>
 			</ThemeProvider>
 		</BrowserRouter>
