a644fae2a138f8ea1ccdcaa660956a71b0fb9d48
Iago <iagobrazmendes@gmail.com>
2021-06-25 20:21:44 -0300

    chore: create youtube embed component

---
+++ src/components/YoutubeEmbed/index.tsx
@@ -0,0 +1,19 @@
+type YoutubeEmbedProps = {
+	embedId: string
+}
+
+export function YoutubeEmbed({embedId}: YoutubeEmbedProps) {
+	return (
+		<div className="video-responsive">
+			<iframe
+				width="853"
+				height="480"
+				src={`https://www.youtube.com/embed/${embedId}`}
+				frameBorder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowFullScreen
+				title="Embedded youtube"
+			/>
+		</div>
+	)
+}
