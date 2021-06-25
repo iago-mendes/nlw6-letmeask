68a275e3c1b7b182b36bc188b2691c8de8be2745
Iago <iagobrazmendes@gmail.com>
2021-06-25 18:05:54 -0300

    chore: setup i18n provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import {BrowserRouter} from 'react-router-dom'
 import {AuthContextProvider} from './contexts/AuthContext'
 import {ThemeProvider} from './contexts/Theme'
 import {Routes} from './Routes'
+import {I18nProvider} from './utils/I18nProvider'
 
 function App() {
 	return (
 		<BrowserRouter>
-			<ThemeProvider>
-				<AuthContextProvider>
-					<Routes />
-				</AuthContextProvider>
-			</ThemeProvider>
+			<I18nProvider>
+				<ThemeProvider>
+					<AuthContextProvider>
+						<Routes />
+					</AuthContextProvider>
+				</ThemeProvider>
+			</I18nProvider>
 		</BrowserRouter>
 	)
 }
---
+++ src/utils/I18nProvider.tsx
@@ -0,0 +1,33 @@
+import React, {ReactNode} from 'react'
+import {i18n} from '@lingui/core'
+import {I18nProvider as LinguiProvider} from '@lingui/react'
+import {
+	detect,
+	fromUrl,
+	fromStorage,
+	fromNavigator
+} from '@lingui/detect-locale'
+
+import enMessages from '../../locales/en/messages.json'
+import ptMessages from '../../locales/pt/messages.json'
+
+i18n.load('en', enMessages)
+i18n.load('pt', ptMessages)
+const DEFAULT_FALLBACK = () => 'en'
+
+type LocaleProviderProps = {
+	children: ReactNode
+}
+
+export function I18nProvider({children}: LocaleProviderProps) {
+	const locale = detect(
+		fromUrl('lang'),
+		fromStorage('lang'),
+		fromNavigator(),
+		DEFAULT_FALLBACK
+	)
+
+	i18n.activate(locale ?? 'en')
+
+	return <LinguiProvider i18n={i18n}>{children}</LinguiProvider>
+}
