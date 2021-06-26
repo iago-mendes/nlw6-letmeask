1854aab50f557e37c7d0e2ee4cf7502adad3a954
Iago <iagobrazmendes@gmail.com>
2021-06-26 12:49:17 -0300

    chore: run lint

--- src/locales/pt/messages.ts
+++ src/locales/pt/messages.ts
@@ -1,1 +1,50 @@
-/*eslint-disable*/export const messages={"Copied!":"Copiado!","Copy room code":"Copiar código da sala","Create a room":"Crie uma sala","Create live Q&A rooms":"Crie salas de Q&A ao vivo","Create room":"Criar sala","Create your room with Google":"Crie sua sala com Google","Do you want to remove this question?":"Você deseja remover essa pergunta?","Do you want to stop?":"Você deseja parar?","End room":"Encerrar sala","Error":"Erro","Google icon":"Ícone do Google","Highlight question":"Destacar pergunta","If you continue, your YouTube live will stop streamming.":"Se você continuar, sua live do YouTube irá parar se ser transmitida.","Illustration of questions and answers":"Ilustração de perguntas e respostas","Join room":"Entrar na sala","Mark as liked":"Marcar como gostei","Mark question as answered":"Marcar pergunta como respondida","Remove question":"Remover pergunta","Room":"Sala","Room #{code}":["Sala #",["code"]],"Room already ended!":"Sala já encerrada!","Room does not exist!":"Essa sala não existe!","Room {title}":["Sala ",["title"]],"Room's name":"Nome da sala","Send question":"Enviar pergunta","Solve your audience questions in real time":"Responda as perguntas da sua audiência em tempo real","Stop live":"Parar live","Stream YouTube live":"Transmitir live do YouTube","The embed ID is informed in the URL after \"/embed/\".":"O embed ID é informado na URL depois de \"/embed/\".","To send a question, <0>log into your account</0>.":"Para enviar uma pergunta, <0>entre na sua conta</0>.","Type the room's code":"Informe o código da sala","Want to join an existing room? <0>click here</0>":"Quer entrar numa sala existente? <0>clique aqui</0>","What do you want to ask?":"O que você deseja perguntar?","What is the embed ID?":"Qual o embed ID?","You are streamming a YouTube live":"Você está transmitindo uma live do YouTube","You cannot undo this action.":"Você não pode desfazer essa ação.","or join a room":"ou entre em uma sala","{0, plural, one {# question} other {# questions}}":[["0","plural",{one:["#"," pergunta"],other:["#"," perguntas"]}]],"{0} question(s)":[["0"]," pergunta(s)"]};
+/*eslint-disable*/ export const messages = {
+	'Copied!': 'Copiado!',
+	'Copy room code': 'Copiar código da sala',
+	'Create a room': 'Crie uma sala',
+	'Create live Q&A rooms': 'Crie salas de Q&A ao vivo',
+	'Create room': 'Criar sala',
+	'Create your room with Google': 'Crie sua sala com Google',
+	'Do you want to remove this question?': 'Você deseja remover essa pergunta?',
+	'Do you want to stop?': 'Você deseja parar?',
+	'End room': 'Encerrar sala',
+	Error: 'Erro',
+	'Google icon': 'Ícone do Google',
+	'Highlight question': 'Destacar pergunta',
+	'If you continue, your YouTube live will stop streamming.':
+		'Se você continuar, sua live do YouTube irá parar se ser transmitida.',
+	'Illustration of questions and answers':
+		'Ilustração de perguntas e respostas',
+	'Join room': 'Entrar na sala',
+	'Mark as liked': 'Marcar como gostei',
+	'Mark question as answered': 'Marcar pergunta como respondida',
+	'Remove question': 'Remover pergunta',
+	Room: 'Sala',
+	'Room #{code}': ['Sala #', ['code']],
+	'Room already ended!': 'Sala já encerrada!',
+	'Room does not exist!': 'Essa sala não existe!',
+	'Room {title}': ['Sala ', ['title']],
+	"Room's name": 'Nome da sala',
+	'Send question': 'Enviar pergunta',
+	'Solve your audience questions in real time':
+		'Responda as perguntas da sua audiência em tempo real',
+	'Stop live': 'Parar live',
+	'Stream YouTube live': 'Transmitir live do YouTube',
+	'The embed ID is informed in the URL after "/embed/".':
+		'O embed ID é informado na URL depois de "/embed/".',
+	'To send a question, <0>log into your account</0>.':
+		'Para enviar uma pergunta, <0>entre na sua conta</0>.',
+	"Type the room's code": 'Informe o código da sala',
+	'Want to join an existing room? <0>click here</0>':
+		'Quer entrar numa sala existente? <0>clique aqui</0>',
+	'What do you want to ask?': 'O que você deseja perguntar?',
+	'What is the embed ID?': 'Qual o embed ID?',
+	'You are streamming a YouTube live':
+		'Você está transmitindo uma live do YouTube',
+	'You cannot undo this action.': 'Você não pode desfazer essa ação.',
+	'or join a room': 'ou entre em uma sala',
+	'{0, plural, one {# question} other {# questions}}': [
+		['0', 'plural', {one: ['#', ' pergunta'], other: ['#', ' perguntas']}]
+	],
+	'{0} question(s)': [['0'], ' pergunta(s)']
+}
